ea201830fe5a71eddc8e4dc72f84c9b215b576b9
Gowtham <ramprasath.meganathan-ext@adient.com>
2018-02-11 11:47:51 +0530

    Intial Commit - Created new Project Anatomy

---
+++ CANADA/CANADA/CANADAPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace CANADA
+{
+    public partial class CANADAPage : ContentPage
+    {
+        public CANADAPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
